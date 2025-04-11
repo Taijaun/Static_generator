095827f01e47e9189438a4f7602deced148f6584
Taijaun Pitt <taijaun@hotmail.co.uk>
2025-04-11 16:36:34 +0100

    /opt/homebrew/bin/python3

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -13,6 +13,24 @@ class TestHTMLNode(unittest.TestCase):
 
         self.assertEqual(node1, node2)
 
+    def test_props_to_html(self):
+        props_dict = {
+            "href": "https://www.google.com",
+            "target": "_blank",
+        }
+        node1 = HTMLNode("p", "hello world", None, props_dict)
+        result = node1.props_to_html()
+        correct_string = f" href=\"https://www.google.com\" target=\"_blank\""
+
+        self.assertNotEqual(correct_string, "")
+        self.assertEqual(correct_string, result)
+
+    def test_props_to_html_no_props(self):
+        node1 = HTMLNode("p", "hello world", None, None)
+        result = node1.props_to_html()
+
+        self.assertEqual(result, "")
+
 
 if __name__ == "__main__":
     unittest.main()
