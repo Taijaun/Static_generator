2e0d4f38d6bf6fa08399d85388d1db1716df6acb
Taijaun Pitt <taijaun@hotmail.co.uk>
2025-04-13 11:47:09 +0100

    Created extraction functions

---
+++ src/test_extractmarkdown.py
@@ -0,0 +1,9 @@
+import unittest
+from  extractmarkdown import *
+
+class TestExtractMarkdown(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
